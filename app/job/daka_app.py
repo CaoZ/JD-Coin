e0be3edc1bba34a8bb4fc0d5f91ffc2f918870ec
CaoZ <caoz@rock-it.org>
2017-02-09 12:53:16 +0800

    dakaActType 会定期变化, 因此改为在页面中获取这个值, 不再每次手动改了.

--- app/job/daka_app.py
+++ app/job/daka_app.py
@@ -8,8 +8,12 @@ class DakaApp(Daka):
     job_name = '京东客户端钢镚打卡'
 
     index_url = 'https://bk.jd.com/m/jdapp/daka/index.html'
-    sign_url = 'https://bk.jd.com/m/jdapp/daka/daka.html?dakaActType=JD_APP_V6'
+    sign_url = 'https://bk.jd.com/m/jdapp/daka/daka.html'
     test_url = 'https://bk.jd.com/m/jdapp/daka/dakDetail.html'
+
+    def __init__(self, session):
+        super().__init__(session)
+        self.daka_act_type = ''
 
     def is_signed(self):
         r = self.session.get(self.index_url)
@@ -28,10 +32,18 @@ class DakaApp(Daka):
                 self.logger.error('返回数据结构可能有变化, 获取打卡数据失败: {}'.format(e))
                 traceback.print_exc()
 
+            if not signed:
+                # no need to parse if already signed
+                self.daka_act_type = self.get_daka_act_type(r.text)
+
+        else:
+            self.logger.error('打卡失败: Status code: {}; Reason: {}'.format(r.status_code, r.reason))
+
         return signed
 
     def sign(self):
-        r = self.session.get(self.sign_url)
+        payload = {'dakaActType': self.daka_act_type}
+        r = self.session.get(self.sign_url, params=payload)
         sign_success = False
 
         if r.ok:
@@ -44,3 +56,13 @@ class DakaApp(Daka):
             self.logger.error('打卡失败: Status code: {}; Reason: {}'.format(r.status_code, r.reason))
 
         return sign_success
+
+    def get_daka_act_type(self, html):
+        # e.g: data:{'dakaActType':'JD_APP_V7'},
+        pattern = r"""dakaActType['"]:\s*['"](.*?)['"]"""
+        daka_act_type = util.find_value(pattern, html)
+
+        if not daka_act_type:
+            self.logger.warning('dakaActType 参数未找到, 页面可能有变化, 打卡可能不成功.')
+
+        return daka_act_type
