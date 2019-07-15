46d63bbfc880f4349b9b716165670134fe0e8c90
songjiang <songjiang@100tal.com>
2019-07-15 14:15:17 +0800

    双签奖励 ok

--- app/job/double_sign.py
+++ app/job/double_sign.py
@@ -10,20 +10,19 @@ from .daka import Daka
 class DoubleSign(Daka):
     job_name = '双签赢奖励'
 
-    index_url = 'https://ljd.m.jd.com/countersign/index.action'
-    sign_url = 'https://ms.jr.jd.com/gw/generic/jrm/h5/m/getAwardList?_=' + \
+    index_url = 'https://ms.jr.jd.com/gw/generic/jrm/h5/m/getAwardList?_=' + \
+        str((int)(time.time()))
+    sign_url = 'https://ms.jr.jd.com/gw/generic/jrm/h5/m/getSignAwardJR?_=' +\
         str((int)(time.time()))
     test_url = index_url
 
     def sign(self):
-        # 参见 https://ljd.m.jd.com/js/countersign/countersign.js
-
         sign_success = True
         message = ''
-
-        if(self.check()):
+        if(self.is_signed()):
             try:
                 res = self.do_sign()
+                print(res)
             except RequestError as e:
                 self.logger.error('双签失败: {}'.format(e.message))
                 return False
@@ -46,25 +45,30 @@ class DoubleSign(Daka):
         self.logger.info('双签成功: {}; Message: {}'.format(sign_success, message))
 
         return sign_success
-    # 完成双签才可访问
-    def is_signed(self):
-        checkUrl = 'https://ms.jr.jd.com/gw/generic/jrm/h5/m/signInit?_=' + \
-            str((int)(time.time()))
-        r = self.session.post(checkUrl)
-        as_json = r.json()
-        # as_json = {'resultCode': 0, 'resultMsg': '操作成功', 'resultData': {'isGet': True, 'isSignInJr': True, 'isSignInJd': True,'resultCode': 200, 'isNewUser': {'resultCode': 200, 'amount': 500, 'isNewUser': 'no', 'resultMsg': '响应成功'}, 'resultMsg': '响应成功', 'longDaySign': True}, 'channelEncrypt': 0}
-        if 'resultData' in as_json and 'resultCode' in as_json['resultData'] and as_json['resultData']['resultCode'] == 200:
-            # 请求成功
-            return as_json['resultData']['isSignInJr'] and as_json['resultData']['isSignInJd']
 
+    def is_signed(self):
         return False
+        sign = False
+        r = self.session.post(self.index_url)
+        as_json = r.json()
+        if 'resultData' in as_json and 'awardList' in as_json['resultData']:
+            award_data = as_json['resultData'].get('awardList')
+            sign = True
+            self.logger.info('今日已签到: {}; 双签奖励: {}'.format(sign, award_data[0]['count']))
+            return sign
+        return sign
 
     def do_sign(self):
         r = self.session.post(self.sign_url)
 
         try:
             as_json = r.json()
-            # print(as_json)
+            #{'resultCode': 0, 'resultMsg': '操作成功',
+            #  'resultData': {'resultCode': 200, '
+            #                   resultMsg': '响应成功', 
+            #                   'status': 0, 'awardList': [
+            #                                       {'count': 5, 'name': '京豆', 'type': 1}]},
+            #                                        'channelEncrypt': 0}
         except ValueError:
             raise RequestError('unexpected response: url: {}; http code: {}'.format(
                 self.sign_url, r.status_code), response=r)
@@ -77,9 +81,3 @@ class DoubleSign(Daka):
             error_msg = as_json.get('message') or str(as_json)
             error_code = as_json.get('businessCode') or as_json.get('code')
             raise RequestError(error_msg, error_code)
-
-    def page_data(self):
-        if not hasattr(self, '_page_data'):
-            self._page_data = self.session.get(self.index_url).text
-
-        return self._page_data
